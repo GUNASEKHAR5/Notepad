9166cf3179a6fe73d0b80861ed8cc861a7c5f9b9
GunaSekhar <kudimigunasekhar@gmail.com>
2025-08-19 19:01:51 +0530

    Main class

--- notepad/src/notepad/Notepad.java
+++ notepad/src/notepad/Notepad.java
@@ -1,10 +1,25 @@
 package notepad;
 
-public class Notepad {
+import javax.swing.*;
+import java.awt.*;
+
+public class Notepad extends JFrame{
+	private static final long serialVersionUID = 1L;
+	
+	Notepad(){
+		setTitle("Notepad");
+		ImageIcon note=new ImageIcon(ClassLoader.getSystemResource("notepad/icons/notepad.png"));
+//		ImageIcon notepadIcon = new ImageIcon(ClassLoader.getSystemResource("notepad/icons/notepad.png"));
+		Image icon=note.getImage();
+		setIconImage(icon);
+		
+		setExtendedState(JFrame.MAXIMIZED_BOTH);
+			
+		setVisible(true);
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		new Notepad();
 	}
 
 }
